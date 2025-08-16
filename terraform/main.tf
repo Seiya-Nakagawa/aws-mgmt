6b707a76b7e3e3520b1f236220b2b87b2b0b0e6d
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-08-16 10:05:18 +0900

    main.tfの中身削除

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,13 @@
 # 共通定義ファイル (データソース, ローカル変数)
 # ----------------------------------------------------------------
 
-# --- データソース: 既存のOrganization/SSOインスタンス情報を取得 ---
-data "aws_organizations_organization" "admin_org" {}
-
-data "aws_ssoadmin_instances" "admin_sso" {}
-
 # --- ローカル変数: 複数のリソースで参照する値を定義 ---
-locals {
-  sso_instance_arn = data.aws_ssoadmin_instances.admin_sso.arns[0]
+# locals {
+#   sso_instance_arn = data.aws_ssoadmin_instances.admin_sso.arns[0]
 
-  # 作成するOUのIDをマップとして管理し、アカウント作成時に参照する
-  ou_ids = {
-    "Workloads-Prd" = aws_organizations_organizational_unit.admin_ou_prd.id
-    "Workloads-Dev" = aws_organizations_organizational_unit.admin_ou_dev.id
-  }
-}
+#   # 作成するOUのIDをマップとして管理し、アカウント作成時に参照する
+#   ou_ids = {
+#     "Workloads-Prd" = aws_organizations_organizational_unit.admin_ou_prd.id
+#     "Workloads-Dev" = aws_organizations_organizational_unit.admin_ou_dev.id
+#   }
+# }
