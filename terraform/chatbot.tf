a7d18619be7a4f1415208fc9dbd0d7134f1c6ffa
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-08-24 16:44:21 +0900

    aaa

--- terraform/chatbot.tf
+++ terraform/chatbot.tf
@@ -1,11 +1,11 @@
-resource "aws_chatbot_slack_channel_configuration" "chatbot_slack" {
-  slack_team_id      = var.slack_team_id
-  slack_channel_id   = var.slack_channel_id
-  configuration_name = "${var.system_name}-${var.env}-chatbot-slack"
-  iam_role_arn       = aws_iam_role.iamrole_chatbot.arn
-  sns_topic_arns     = [aws_sns_topic.sns_topic_awschat.arn]
+# resource "aws_chatbot_slack_channel_configuration" "chatbot_slack" {
+#   slack_team_id      = var.slack_team_id
+#   slack_channel_id   = var.slack_channel_id
+#   configuration_name = "${var.system_name}-${var.env}-chatbot-slack"
+#   iam_role_arn       = aws_iam_role.iamrole_chatbot.arn
+#   sns_topic_arns     = [aws_sns_topic.sns_topic_awschat.arn]
 
-  # ログレベルを設定 (ERROR, INFO, NONE)
-  logging_level = "ERROR" 
-  depends_on = [aws_cloudwatch_log_group.loggroup_chatbot]
-}
+#   # ログレベルを設定 (ERROR, INFO, NONE)
+#   logging_level = "ERROR" 
+#   depends_on = [aws_cloudwatch_log_group.loggroup_chatbot]
+# }
