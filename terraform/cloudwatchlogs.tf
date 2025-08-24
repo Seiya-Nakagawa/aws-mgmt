b018db6a679a01c7f82660e1d383ec2b3c2260c6
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-08-24 16:04:07 +0900

    aaa

--- terraform/cloudwatchlogs.tf
+++ terraform/cloudwatchlogs.tf
@@ -2,10 +2,10 @@
 
 # Chatbot用のCloudWatchロググループ
 resource "aws_cloudwatch_log_group" "loggroup_chatbot" {
-  name              = "/aws/chatbot/chat-configurations/${aws_chatbot_slack_channel_configuration.chatbot_slack.name}"
+  name              = "/aws/chatbot/chat-configurations/${var.system_name}-${var.env}-chatbot-slack"
   retention_in_days = 1
   tags = {
-    Name            = "/aws/chatbot/chat-configurations/${aws_chatbot_slack_channel_configuration.chatbot_slack.name}",
+    Name            = "/aws/chatbot/chat-configurations/${var.system_name}-${var.env}-chatbot-slack",
     SystemName      = var.system_name,
     Env             = var.env,
   }
@@ -13,10 +13,10 @@ resource "aws_cloudwatch_log_group" "loggroup_chatbot" {
 
 # SNSの配信ステータスログを保存するCloudWatch Log Group (aws_sns_topic.sns_topic_awschat用)
 resource "aws_cloudwatch_log_group" "sns_topic_awschat_log_group" {
-  name              = "sns/${var.aws_region}/${var.aws_account_id}/${aws_sns_topic.sns_topic_awschat.name}"
+  name              = "sns/${var.aws_region}/${var.aws_account_id}/${var.system_name}-${var.env}-sns-chatbot"
   retention_in_days = 1
   tags = {
-    Name            = "sns/${var.aws_region}/${var.aws_account_id}/${aws_sns_topic.sns_topic_awschat.name}",
+    Name            = "sns/${var.aws_region}/${var.aws_account_id}/${var.system_name}-${var.env}-sns-chatbot",
     SystemName      = var.system_name,
     Env             = var.env,
   }
