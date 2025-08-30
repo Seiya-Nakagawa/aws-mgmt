f157680bfbd04c3b8cbd5f64fcf2f14b459b47d3
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-08-30 11:27:33 +0900

    ccc

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,27 +9,3 @@ resource "aws_accessanalyzer_analyzer" "accessanaly" {
     Env         = var.env,
   }
 }
-
-# SNSサービスが引き受けるためのIAMロール
-resource "aws_iam_role" "sns_delivery_status_logging_role" {
-  name               = "sns-delivery-status-logging-role"
-  description        = "Role for SNS to log delivery status to CloudWatch"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "sns.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-# 作成したポリシーをロールにアタッチ
-resource "aws_iam_role_policy_attachment" "sns_delivery_status_logging_attachment" {
-  role       = aws_iam_role.sns_delivery_status_logging_role.name
-  policy_arn = aws_iam_policy.sns_delivery_status_logging_policy.arn
-}
