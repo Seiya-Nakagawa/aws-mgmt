702cf61561a978db6f3bebf2fb510377a131755c
Seiya-Nakagawa <aibdlnew1.work@gmail.com>
2025-08-31 09:21:09 +0900

    １１１

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,8 +13,6 @@ variable "env" {
   type        = string
 }
 
-
-
 variable "aws_account_id" {
   description = "AWSアカウントID"
   type        = string
@@ -35,5 +33,10 @@ variable "budget_thresholds" {
   description = "予算アラートを通知するしきい値（パーセンテージ）のリスト"
   type        = list(number)
   default     = []
+}
+
+variable "sso_user_ids" {
+  type        = list(string)
+  description = "SSMパラメータから情報を取得するためのSSOユーザーIDのリスト"
 }
 
